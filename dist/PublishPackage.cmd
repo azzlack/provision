937fe41e0067d238ba00430768cf98679b852f8b
Ove Andersen <ove.andersen@outlook.com>
2014-08-11 16:11:56 +0200

    Created nuget package and cleaned out some files

---
+++ dist/PublishPackage.cmd
@@ -0,0 +1,12 @@
+@echo off
+set /P key= Enter NuGet API Key:
+echo
+echo Updating NuGet
+nuget.exe update -self
+echo
+echo Publishing Packages
+nuget push Provision.1.0.1.nupkg -ApiKey %key%
+nuget push Provision.Config.1.0.0.nupkg -ApiKey %key%
+nuget push Provision.Providers.MemoryCache.1.0.0.nupkg -ApiKey %key%
+nuget push Provision.Providers.Redis.1.0.0.nupkg -ApiKey %key%
+nuget push Provision.Providers.PortableMemoryCache.1.0.0.nupkg -ApiKey %key%
