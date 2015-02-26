08a3d3bb67e832f15ceb65d74c42b96625266948
Ove Andersen <ove.andersen@outlook.com>
2015-02-26 13:41:39 +0100

    Created new release

--- dist/PublishPackage.cmd
+++ dist/PublishPackage.cmd
@@ -8,6 +8,6 @@ echo.
 echo Publishing Packages
 nuget push Provision.1.4.1.nupkg -ApiKey %key%
 nuget push Provision.Config.1.4.1.nupkg -ApiKey %key%
-nuget push Provision.Providers.MemoryCache.1.4.1.nupkg -ApiKey %key%
+nuget push Provision.Providers.MemoryCache.1.4.2.nupkg -ApiKey %key%
 nuget push Provision.Providers.Redis.1.4.1.nupkg -ApiKey %key%
 nuget push Provision.Providers.PortableMemoryCache.1.4.1.nupkg -ApiKey %key%
